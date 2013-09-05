18eaeebd292bb40d7aa5b9e6b6ca81d4786e1884
Stefan Profanter <git@s.profanter.me>
2013-09-05 16:08:50 +0200

    Added radial offest

--- cognitivesystems/pro_gripperbot/src/cylinder_grip.cpp
+++ cognitivesystems/pro_gripperbot/src/cylinder_grip.cpp
@@ -18,6 +18,8 @@ struct Ros_Localizer {
 
 
 	uint8_t gripped;
+	bool grasped;
+	geometry_msgs::Pose oldRobotPose;
 };
 
 struct Ros_Localizer *loc;
@@ -72,123 +74,173 @@ void trackerCallback(const ros_tum_msgs::ActorVec& actorArr)
 	ROS_INFO("\tPose Green: %f|%f|%f", poseGreen.position.x,poseGreen.position.y,poseGreen.position.z );
 
     std::cout << __LINE__ << std::endl;
+    robot_control::MoveToOS moveTo;
 
-	robot_control::MoveToOS moveTo;
-	moveTo.request.x = poseYellow.position.x-0.03;
-	moveTo.request.y = poseYellow.position.y;
-	moveTo.request.z = poseYellow.position.z+0.1;
-	moveTo.request.effector = "gripper";
-	if (!loc->moveToOs.call(moveTo))
-	{
-		ROS_ERROR("Failed to call service MoveToOS over yellow");
-		return;
-	}
-	if (moveTo.response.success == 0) {
+    if(!loc->grasped)
+    {
 
-		ROS_ERROR("MoveToOS over yellow failed, success is 0!");
-		return;
-	}
+		moveTo.request.x = poseYellow.position.x-0.03;
+		moveTo.request.y = poseYellow.position.y;
+		moveTo.request.z = poseYellow.position.z+0.1;
+		moveTo.request.effector = "gripper";
 
-	moveTo.request.z = poseYellow.position.z;
-	if (!loc->moveToOs.call(moveTo))
-	{
-		ROS_ERROR("Failed to call service MoveToOS at yellow");
-		return;
-	}
-	if (moveTo.response.success == 0) {
+		loc->oldRobotPose.position.x = poseYellow.position.x;
+		loc->oldRobotPose.position.y = poseYellow.position.y;
+		loc->oldRobotPose.position.z = poseYellow.position.z;
 
-		ROS_ERROR("MoveToOS at yellow failed, success is 0!");
-		return;
-	}
+		if (!loc->moveToOs.call(moveTo))
+		{
+			ROS_ERROR("Failed to call service MoveToOS over yellow");
+			return;
+		}
+		if (moveTo.response.success == 0) {
 
-	gripper_control::CloseGripper grpClose;
-	if (!loc->closeGripper.call(grpClose))
-	{
-		ROS_ERROR("Failed to call service close_gripper");
-		return;
-	}
+			ROS_ERROR("MoveToOS over yellow failed, success is 0!");
+			return;
+		}
 
-	if (grpClose.response.success == 0) {
+		moveTo.request.z = poseYellow.position.z;
+		if (!loc->moveToOs.call(moveTo))
+		{
+			ROS_ERROR("Failed to call service MoveToOS at yellow");
+			return;
+		}
+		if (moveTo.response.success == 0) {
 
-		ROS_ERROR("Gripper Close failed, success is 0!");
-		return;
-	}
+			ROS_ERROR("MoveToOS at yellow failed, success is 0!");
+			return;
+		}
 
-	moveTo.request.z = poseYellow.position.z+0.1;
-	if (!loc->moveToOs.call(moveTo))
-	{
-		ROS_ERROR("Failed to call service MoveToOS over yellow gripped");
-		return;
-	}
-	if (moveTo.response.success == 0) {
+		gripper_control::CloseGripper grpClose;
+		if (!loc->closeGripper.call(grpClose))
+		{
+			ROS_ERROR("Failed to call service close_gripper");
+			return;
+		}
 
-		ROS_ERROR("MoveToOS over yellow gripped failed, success is 0!");
-		return;
-	}
+		if (grpClose.response.success == 0) {
 
-	/* Move to green box */
-	moveTo.request.x = poseGreen.position.x-0.03;
-	moveTo.request.y = poseGreen.position.y;
-	moveTo.request.z = poseGreen.position.z+0.1;
-	moveTo.request.effector = "gripper";
-	if (!loc->moveToOs.call(moveTo))
-	{
-		ROS_ERROR("Failed to call service MoveToOS over green");
-		return;
-	}
-	if (moveTo.response.success == 0) {
+			ROS_ERROR("Gripper Close failed, success is 0!");
+			return;
+		}
+		loc->grasped = true;
 
-		ROS_ERROR("MoveToOS over green failed, success is 0!");
 		return;
 	}
 
-	moveTo.request.x = poseGreen.position.x-0.03;
-	moveTo.request.y = poseGreen.position.y;
-	moveTo.request.z = poseGreen.position.z+0.027;
-	moveTo.request.effector = "gripper";
-	if (!loc->moveToOs.call(moveTo))
-	{
-		ROS_ERROR("Failed to call service MoveToOS at green");
-		return;
-	}
-	if (moveTo.response.success == 0) {
-
-		ROS_ERROR("MoveToOS at green failed, success is 0!");
-		return;
-	}
-
-	gripper_control::OpenGripper grpOpen;
-	if (!loc->openGripper.call(grpOpen))
-	{
-		ROS_ERROR("Failed to call service open gripper");
-		return;
-	}
-
-	if (grpOpen.response.success == 0) {
-
-		ROS_ERROR("Gripper open failed, success is 0!");
-		return;
-	}
+    else
+    {
+    	moveTo.request.y = poseYellow.position.y;
+    	moveTo.request.x = poseYellow.position.x-0.03;
+		moveTo.request.z = poseYellow.position.z+0.1;
+		moveTo.request.effector = "gripper";
 
 
-	moveTo.request.x = 0.3;
-	moveTo.request.y = 0;
-	moveTo.request.z = 0.3;
-	if (!loc->moveToOs.call(moveTo))
-	{
-		ROS_ERROR("Failed to call service MoveToOS end");
-		return;
-	}
-	if (moveTo.response.success == 0) {
 
-		ROS_ERROR("MoveToOS end failed, success is 0!");
-		return;
-	}
+		if (!loc->moveToOs.call(moveTo))
+		{
+			ROS_ERROR("Failed to call service MoveToOS over yellow gripped");
+			return;
+		}
+		if (moveTo.response.success == 0) {
 
-	ROS_INFO("Gripping successfully finished!");
-	loc->gripped = 1;
+			ROS_ERROR("MoveToOS over yellow gripped failed, success is 0!");
+			return;
+		}
+
+		/* Move to green box */
+		//calculate offset after grapping
+
+		double posOffsetX = poseYellow.position.x - loc->oldRobotPose.position.x;
+		double posOffsetY = poseYellow.position.y - loc->oldRobotPose.position.y;
+
+		double robotPosRadial = sqrt(pow(loc->oldRobotPose.position.x, 2) + pow(loc->oldRobotPose.position.y, 2) );
+		double offsetRadial = sqrt(pow(posOffsetX, 2) + pow(posOffsetY, 2) );
+		double offsetRadialProjected;
+		if (offsetRadial < 0.001) {
+			offsetRadialProjected = 0;
+	    	ROS_INFO("Projected offset too small, using the default radial offset value");
+		}
+		else
+		{
+			offsetRadialProjected =
+					offsetRadial*(posOffsetX*loc->oldRobotPose.position.x + posOffsetY*loc->oldRobotPose.position.y)/
+					(offsetRadial*robotPosRadial);
+			ROS_INFO("Projected offset (radial length): %.2f mm", offsetRadialProjected*1000.);
+		}
+
+		double correctionOffset = -0.010;
+
+		double newRobotPosAngle = atan2(poseGreen.position.y, poseGreen.position.x);
+		ROS_INFO("Green angle: %.2f deg", newRobotPosAngle*180./3.1415926);
+
+		double newPosOffsetX = correctionOffset - offsetRadialProjected*cos(newRobotPosAngle);
+		double newPosOffsetY = correctionOffset - offsetRadialProjected*sin(newRobotPosAngle);
+
+		ROS_INFO("Used offset X: %.2f mm", newPosOffsetX*1000);
+		ROS_INFO("Used offset Y: %.2f mm", newPosOffsetY*1000);
+
+		moveTo.request.x = poseGreen.position.x - newPosOffsetX;
+		moveTo.request.y = poseGreen.position.y - newPosOffsetY;
+		moveTo.request.z = poseGreen.position.z+0.1;
+		if (!loc->moveToOs.call(moveTo))
+		{
+			ROS_ERROR("Failed to call service MoveToOS over green");
+			return;
+		}
+		if (moveTo.response.success == 0) {
+
+			ROS_ERROR("MoveToOS over green failed, success is 0!");
+			return;
+		}
+
+		moveTo.request.x = poseGreen.position.x-0.03-posOffsetX;
+		moveTo.request.y = poseGreen.position.y-posOffsetY;
+		moveTo.request.z = poseGreen.position.z+0.027;
+		moveTo.request.effector = "gripper";
+		if (!loc->moveToOs.call(moveTo))
+		{
+			ROS_ERROR("Failed to call service MoveToOS at green");
+			return;
+		}
+		if (moveTo.response.success == 0) {
+
+			ROS_ERROR("MoveToOS at green failed, success is 0!");
+			return;
+		}
+
+		gripper_control::OpenGripper grpOpen;
+		if (!loc->openGripper.call(grpOpen))
+		{
+			ROS_ERROR("Failed to call service open gripper");
+			return;
+		}
+
+		if (grpOpen.response.success == 0) {
+
+			ROS_ERROR("Gripper open failed, success is 0!");
+			return;
+		}
 
 
+		moveTo.request.x = 0.3;
+		moveTo.request.y = 0;
+		moveTo.request.z = 0.3;
+		if (!loc->moveToOs.call(moveTo))
+		{
+			ROS_ERROR("Failed to call service MoveToOS end");
+			return;
+		}
+		if (moveTo.response.success == 0) {
+
+			ROS_ERROR("MoveToOS end failed, success is 0!");
+			return;
+		}
+
+		ROS_INFO("Gripping successfully finished!");
+		loc->gripped = 1;
+
+    }
 
 
 }
@@ -199,9 +251,10 @@ int main(int argc, char **argv)
 
 	Ros_Localizer l;
 	l.gripped = 0;
+	l.grasped = false;
 	loc = &l;
 
-	ros::Subscriber sub = loc->n.subscribe("/object_detector/objects_data", 2, trackerCallback);
+	ros::Subscriber sub = loc->n.subscribe("/object_detector/objects_data", 1, trackerCallback);
 
 	loc->closeGripper = loc->n.serviceClient<gripper_control::CloseGripper>("/gripper_control/close_gripper");
 	loc->openGripper = loc->n.serviceClient<gripper_control::OpenGripper>("/gripper_control/open_gripper");
